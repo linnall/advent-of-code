3fc4ed88cce6a5bfa53c113fc307b07d3fa99ca5
Linna Luo <linnaluo18@gmail.com>
2022-12-19 00:31:02 -0500

    modifies day1 soln, adds day 2 soln

--- 2022/1a.py
+++ 2022/1a.py
@@ -1,11 +1,10 @@
 def solution():
-    maxCals = 0
+    elfCals = []
     with open('1.in', 'r') as file:
         elfCals = file.read().split('\n\n')
-        for elfCal in elfCals:
-            elfCal = [int(e) for e in elfCal.split('\n')]
-            maxCals = max(maxCals, sum(elfCal))
-    return maxCals
+        elfCals = map(lambda s: sum([int(e) for e in s.split('\n')]), elfCals)
+        elfCals = list(elfCals)
+    return max(elfCals)
 
 
 print(solution())
