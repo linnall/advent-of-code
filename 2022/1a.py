d27f154e1efbc035ec0b10cd6f57b0e0769f93bc
Linna Luo <linnaluo18@gmail.com>
2023-10-29 18:04:25 -0400

    removes empty files + fixes merge conflicts

--- 2022/1a.py
+++ 2022/1a.py
@@ -1,20 +1,10 @@
 def solution():
-<<<<<<< HEAD
-    maxCals = 0
-    with open('1.in', 'r') as file:
-        elfCals = file.read().split('\n\n')
-        for elfCal in elfCals:
-            elfCal = [int(e) for e in elfCal.split('\n')]
-            maxCals = max(maxCals, sum(elfCal))
-    return maxCals
-=======
     elfCals = []
-    with open('1.in', 'r') as file:
-        elfCals = file.read().split('\n\n')
-        elfCals = map(lambda s: sum([int(e) for e in s.split('\n')]), elfCals)
+    with open("1.in", "r") as file:
+        elfCals = file.read().split("\n\n")
+        elfCals = map(lambda s: sum([int(e) for e in s.split("\n")]), elfCals)
         elfCals = list(elfCals)
     return max(elfCals)
->>>>>>> main
 
 
 print(solution())
