4397d67b23006a07a59a0404fb76a81bd0ff1667
Linna Luo <linnaluo18@gmail.com>
2023-10-29 17:52:53 -0400

    adds day 10 part 1 solution

---
+++ 2022/10b.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+
+def solution():
+    with open("10.in", "r") as file:
+        instructions = deque(file.read().split("\n"))
+        X = 1
+        target = 39
+        buffer = deque()
+        for i in range(240):
+            if i >= X - 1 and i <= X + 1:
+                print("#", end="")
+            else:
+                print(".", end="")
+            if i == target:
+                print("\n", end="")
+                target += 40
+            if instructions:
+                line = instructions.popleft().split()
+                if line[0] == "addx":
+                    buffer.extend([0, int(line[1])])
+                else:
+                    buffer.append(0)
+            if buffer:
+                X += buffer.popleft()
+
+
+solution()
