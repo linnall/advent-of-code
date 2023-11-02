0955b686d0b01bf00232a2259ee99bb064f9d3a5
Linna Luo <linnaluo18@gmail.com>
2023-11-02 01:48:57 -0400

    adds initial day 12 part 1 WORKING solution with a lot of print statements hehe

--- 2022/12a.py
+++ 2022/12a.py
@@ -12,14 +12,25 @@ def print_grid(grid):
 
 def bfs(grid, start, end):
     seen = set()
-    queue = deque([[start, 0]])
+    queue = deque([start])
     pathLen = 0
+    levelNum = len(queue)
     while queue:
         # print(queue)
-        [row, col], d = queue.popleft()
+        row, col = queue.popleft()
+        levelNum -= 1
         if row == end[0] and col == end[1]:
-            return d
+            print(row, col)
+            print("RETURN")
+            return pathLen
         if (row, col) in seen:
+            if levelNum == 0:
+                print("LEVEL 0")
+                print(queue)
+                print(seen)
+                levelNum = len(queue)
+                print(levelNum)
+                pathLen += 1
             continue
         seen.add((row, col))
         # if grid[row][col] == ".":
@@ -28,27 +39,36 @@ def bfs(grid, start, end):
         currElevation = ord(grid[row][col])
         if row > 0:
             if currElevation + 1 >= ord(grid[row - 1][col]):
-                queue.append([[row - 1, col], d + 1])
+                queue.append([row - 1, col])
             # else:
             #     grid[row - 1][col] = "."
         if row < len(grid) - 1:
             if currElevation + 1 >= ord(grid[row + 1][col]):
-                queue.append([[row + 1, col], d + 1])
+                queue.append([row + 1, col])
             # else:
             #     grid[row + 1][col] = "."
         if col > 0:
             if currElevation + 1 >= ord(grid[row][col - 1]):
-                queue.append([[row, col - 1], d + 1])
+                queue.append([row, col - 1])
             # else:
             #     grid[row][col - 1] = "."
         if col < len(grid[0]) - 1:
             if currElevation + 1 >= ord(grid[row][col + 1]):
-                queue.append([[row, col + 1], d + 1])
+                queue.append([row, col + 1])
             # else:
             #     grid[row][col + 1] = "."
-        # grid[row][col] = "."  # mark current cell as visited
+        grid[row][col] = "."  # mark current cell as visited
         print_grid(grid)
-        grid[row][col] = "."
+        print(queue)
+        print(seen)
+        print(levelNum)
+        if levelNum == 0:
+            print("LEVEL 0")
+            print(queue)
+            print(seen)
+            levelNum = len(queue)
+            print(levelNum)
+            pathLen += 1
 
 
 def solution():
@@ -66,6 +86,8 @@ def solution():
             elif grid[i][j] == "E":
                 end = [i, j]
                 grid[i][j] = "z"
+    print(start)
+    print(end)
 
     # run bfs
     return bfs(grid, start, end)
