b24b892d68cd34d63bb8e1ac98a5aa4af723c835
Linna Luo <linnaluo18@gmail.com>
2023-11-08 11:05:12 -0500

    adds day 13 part 1 solution THAT WORKS

--- 2022/13a.py
+++ 2022/13a.py
@@ -15,33 +15,33 @@ def buildList(queue):
             lst.append(int(ch))
 
 
-def compare(list1, list2):
-    while list1 and list2:
-        l1 = list1.popleft()
-        l2 = list2.popleft()
-        if type(l1) is int and type(l2) is int:
-            if l1 < l2:
-                return 1
-            elif l1 > l2:
-                return -1
-            else:
-                return 0
-        elif type(l1) is list and type(l2) is list:
-            ret = compare(deque(l1), deque(l2))
+def compare(l1, l2):
+    if type(l1) is int and type(l2) is int:
+        if l1 < l2:
+            return 1
+        elif l1 > l2:
+            return -1
+        else:
+            return 0
+    elif type(l1) is list and type(l2) is list:
+        i = 0
+        while i < len(l1) and i < len(l2):
+            ret = compare(l1[i], l2[i])
             if ret == 1:
                 return 1
+            elif ret == -1:
+                return -1
+            i += 1
+        if i == len(l1) and i == len(l2):
+            return 0
+        elif i == len(l2):
+            return -1
         else:
-            if type(l1) is int:
-                l1 = [l1]
-            else:
-                l2 = [l2]
-            list1.appendleft(l1)
-            list2.appendleft(l2)
-    if list2:
-        return 1
-    elif list1:
-        return -1
-    return 0  # if the lists are the same
+            return 1
+    elif type(l1) is int:
+        return compare([l1], l2)
+    else:
+        return compare(l1, [l2])
 
 
 def solution():
@@ -50,16 +50,21 @@ def solution():
         inOrder = 0
         for i, pair in enumerate(pairs):
             lists = pair.split("\n")
-            list1String = deque(list(lists[0]))
-            # remove first lbracket
-            list1String.popleft()
-            list1 = buildList(list1String)
-            list2String = deque(list(lists[1]))
-            # remove first lbracket
-            list2String.popleft()
-            list2 = buildList(list2String)
+            # list1String = deque(list(lists[0]))
+            # # remove first lbracket
+            # list1String.popleft()
+            # list1 = buildList(list1String)
+            # list2String = deque(list(lists[1]))
+            # # remove first lbracket
+            # list2String.popleft()
+            # list2 = buildList(list2String)
             # compare
-            if compare(deque(list1), deque(list2)) == 1:
+            list1 = eval(lists[0])
+            list2 = eval(lists[1])
+            print(list1)
+            print(list2)
+            if compare(list1, list2) == 1:
+                print(i + 1)
                 inOrder += i + 1
         return inOrder
 
