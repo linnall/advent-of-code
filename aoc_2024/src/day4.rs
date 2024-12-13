c01a1b58c4de6da4e9d9e32c4d3c30d37e72b40b
linna <linna@alchemy.com>
2024-12-12 20:26:23 -0500

    adds day 4 part 1 solution

--- aoc_2024/src/day4.rs
+++ aoc_2024/src/day4.rs
@@ -6,9 +6,9 @@ fn build_grid(file_path: &str) -> Vec<Vec<String>> {
     match fs::read_to_string(file_path) {
         Ok(contents) => {
             grid = contents
-            .lines()
-            .map(|line| line.chars().map(|ch| ch.to_string()).collect())
-            .collect();
+                .lines()
+                .map(|line| line.chars().map(|ch| ch.to_string()).collect())
+                .collect();
         }
         Err(e) => {
             eprintln!("Error reading file: {}", e);
@@ -21,22 +21,31 @@ fn build_grid(file_path: &str) -> Vec<Vec<String>> {
     grid
 }
 
-fn xmas_count(x: usize, y: usize, grid: &Vec<Vec<String>>) -> i32 {
+fn xmas_count(row: usize, col: usize, grid: &Vec<Vec<String>>) -> i32 {
     let rows = grid.len();
     let cols = grid[0].len();
     let mut total = 0;
-    let directions: Vec<(i32, i32)> = Vec::from([(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)]);
-    let a = x;
-    let b = y;
-    for (row, col) in directions {
-        let mut string = String::from(grid[x][y]);
+    let directions: Vec<(i32, i32)> = Vec::from([
+        (0, 1),
+        (-1, 1),
+        (-1, 0),
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]);
+    for (ud, lr) in directions {
+        let mut _row: i32 = row as i32;
+        let mut _col: i32 = col as i32;
+        let mut string = String::from(&grid[_row as usize][_col as usize]);
         for _ in 0..3 {
-            let _a = a as i32 + row;
-            let _b = b as i32 + col;
-            if _a < 0 || a > rows || b < 0 || b > cols {
+            _row += ud;
+            _col += lr;
+            if _row < 0 || _row >= rows as i32 || _col < 0 || _col >= cols as i32 {
                 break;
             }
-            let ch = grid[_a as usize][_b as usize];
+            let ch = &grid[_row as usize][_col as usize];
             if string == "X" && ch == "M" {
                 string.push('M');
             } else if string == "XM" && ch == "A" {
@@ -54,10 +63,11 @@ fn xmas_count(x: usize, y: usize, grid: &Vec<Vec<String>>) -> i32 {
 pub fn part_one() {
     let file_path = "4.in";
     let grid = build_grid(file_path);
-    for row in &grid {
-        for element in row {
-
+    let mut total = 0;
+    for row in 0..grid.len() {
+        for col in 0..grid[row].len() {
+            total += xmas_count(row, col, &grid);
         }
     }
+    println!("{total}");
 }
-
